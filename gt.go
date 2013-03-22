d22dcc1da9903979fcc29cb578dbc2ee4baaa3c5
Jesper <jumziey@gmail.com>
2013-03-22 20:12:37 +0100

    Very small bikeshed issue

--- gt.go
+++ gt.go
@@ -1,28 +1,35 @@
 package main
 
-import(
-	"fmt"
+import (
 	"bittor"
 	"errors"
-	"log"
-	"regexp"
-	"os"
-	"net/url"
-	"net/http"
+	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"regexp"
+	"hash"
 )
 
+type torrentInfo struct {
+	id hash.Hash
+	complete int
+	downloaded int
+	incomplete int
+}
 
 //Returns "" if tracker does not support scrape
 //i.e. the announce does not have an announce after
 //the last '/'
-func announceToScrape(aUrl* url.URL) *url.URL {
+func announceToScrape(aUrl *url.URL) *url.URL {
 
 	rAnnounceCheck, err := regexp.Compile("(/announce)([^/]*$)")
 	if err != nil {
 		log.Fatalln("UPSTREAM REGEXP BROKEN - REPORT!")
 	}
-	
+
 	a := aUrl.String()
 	if !rAnnounceCheck.MatchString(a) {
 		return nil
@@ -30,84 +37,105 @@ func announceToScrape(aUrl* url.URL) *url.URL {
 	sUrl, err := url.Parse(rAnnounceCheck.ReplaceAllString(a, "/scrape$2"))
 	if err != nil {
 		return nil
-	}	
+	}
 	return sUrl
- }
- 
- //Takes the master dict and returns a suitable announce url as a Url
- func getAnnounce(m map[string]interface{}) (*url.URL, error) {
- 	//Should check if the announce tracker works go through the announce list
- 	//until we find someone that works, will now only return the main announce 
- 	//tracker no matter what.
- 	
- 	announce := m["announce"].(string)
- 	if announce == "" {
- 		return nil, errors.New("Found no annonuce in master dict")
- 	}
- 	
- 	aUrl, err := url.Parse(announce)
- 	if err != nil {
- 		return nil, errors.New(fmt.Sprint("Error url parsing announce: ", err))
- 	}
- 	
- 	return aUrl, nil
- }
- 
+}
+
+//Takes the master dict and returns a suitable announce url as a url.Url
+func getAnnounce(m map[string]interface{}) (*url.URL, error) {
+	//Should check if the announce tracker works go through the announce list
+	//until we find someone that works, will now only return the main announce 
+	//tracker no matter what.
+
+	announce := m["announce"].(string)
+	if announce == "" {
+		return nil, errors.New("Found no annonuce in master dict")
+	}
+
+	aUrl, err := url.Parse(announce)
+	if err != nil {
+		return nil, errors.New(fmt.Sprint("Error url parsing announce: ", err))
+	}
+
+	return aUrl, nil
+}
+
+func scrape(aUrl *url.URL,info_hash hash.Hash) ([]byte,error) {
+	sUrl := announceToScrape(aUrl)
+	if sUrl == nil {
+		return nil, errors.New("It would seem there is no scrape convention supported")
+	}
+
+	v := url.Values{}
+	v.Set("info_hash", string(info_hash.Sum(nil)))
+	sUrl.RawQuery = v.Encode()
+	aUrl.RawQuery = v.Encode()
+
+	sResp, err := http.Get(sUrl.String())
+	if err != nil {
+		return nil, err
+	}
+	defer sResp.Body.Close()
+
+	scrape_body, err := ioutil.ReadAll(sResp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return scrape_body, nil
+}
+
 func main() {
-	fmt.Println("start")
-	f,err := os.Open("daily.torrent")
+	torrent := torrentInfo{};
+
+	fmt.Println("############start############")
+	fmt.Println()
+	torrentFile := "hunter.torrent"
+
+	f, err := os.Open(torrentFile)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer f.Close()
-	
-	s, err := bittor.GetMainDict(f)
+
+	src, err := bittor.GetMainDict(f)
 	if err != nil {
-		log.Fatalln("Error decoding ", "daily.torrent", ":", err)
+		log.Fatalln("Error decoding ", torrentFile, ":", err)
 	}
-	info := bittor.GetInfoDict(s)
+	info := bittor.GetDict(src, "info")
 	if info == nil {
 		log.Fatalln("Found no info dict in torrent file")
 	}
-	
+
 	//Some reading must have been done in bittor.GetMainDict()
-	f.Seek(0,0)
+	f.Seek(0, 0)
 	info_hash, err := bittor.InfoHash(f)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	
-	aUrl, err := getAnnounce(s)
-	if aUrl == nil {
-		log.Fatalln(err)
-	}
-	sUrl := announceToScrape(aUrl)
-	if sUrl == nil {
-		log.Fatalln("It would seem there is no scrape convention supported")
-	}
-	
-	v := url.Values{}
-	v.Set("info_hash", url.QueryEscape(string(info_hash.Sum(nil))) )
-	sUrl.RawQuery = v.Encode()
-	aUrl.RawQuery = v.Encode()
-	
-	fmt.Println()
-	fmt.Println(fmt.Sprintf("%x", info_hash.Sum(nil)))
-	fmt.Println()
-	fmt.Println(sUrl.String())
-	fmt.Println()
-	
-	sResp, err := http.Get(sUrl.String())
+
+	aUrl, err := getAnnounce(src);
 	if err != nil {
 		log.Fatalln(err)
 	}
-	//defer sResp.Body.Close()
 	
+	scrape_body, err := scrape(aUrl, info_hash);
+	if err != nil {
+		log.Fatalln(err)
+	}
 	
-	bleh, _ := ioutil.ReadAll(sResp.Body)
-	fmt.Println(string(bleh))
+	b_data, err := bittor.GetDictFromByte(scrape_body);
+	if err != nil {
+		log.Fatalln(err)
+	}
 	
+	actual_torrent := bittor.GetDict(b_data, "files")
+	hashy := bittor.GetDict(actual_torrent, string(info_hash.Sum(nil)))
+	
+	torrent.id = info_hash;
+	torrent.complete = hashy["complete"].(int)
+	torrent.incomplete = hashy["incomplete"].(int)
+	torrent.downloaded = hashy["downloaded"].(int)
 
-	
-
+	fmt.Println(torrent.complete);
 }
+

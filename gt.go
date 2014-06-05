ec163b35f4eb9283d71dec920590468d285b0be0
Jesper Vesterberg <jumziey@gmail.com>
2014-06-05 15:38:35 +0200

    Just about to send my first proper announce when i realize that we're communicating over the udp protocol... sigh... Hopefully no large changes have to be made.

--- gt.go
+++ gt.go
@@ -10,6 +10,10 @@ import (
 	"net/url"
 	"regexp"
 	"hash"
+	"time"
+	"strconv"
+	"crypto/rand"
+	"flag"
 )
 
 type torrentInfo struct {
@@ -19,6 +23,34 @@ type torrentInfo struct {
 	incomplete int
 }
 
+var version string = "0001"
+
+var port = flag.String("p", "6885", "Sets Client P2P Port")
+
+func peerIdCreation() []byte {
+
+	peer_id := make([]byte,0)
+	peer_id = append(peer_id, []byte("GT")...)
+	peer_id = append(peer_id, []byte(version)...)
+	peer_id = append(peer_id, []byte("-")...)
+	
+	t := uint16(time.Now().Nanosecond())
+	if( t<100) { //must have 3 significant time digits 
+		t+=100
+	}
+	ts := strconv.FormatInt(int64(t),10)
+	peer_id = append(peer_id, []byte(ts[0:3])...)
+	
+	//Add 10 more completly random bytes to peer_id
+	r := make([]byte,10)
+	rand.Read(r)
+	peer_id = append(peer_id,r...)
+	
+	return peer_id
+}
+
+
+
 //Returns "" if tracker does not support scrape
 //i.e. the announce does not have an announce after
 //the last '/'
@@ -26,7 +58,7 @@ func announceToScrape(aUrl *url.URL) *url.URL {
 
 	rAnnounceCheck, err := regexp.Compile("(/announce)([^/]*$)")
 	if err != nil {
-		log.Fatalln("UPSTREAM REGEXP BROKEN - REPORT!")
+		log.Fatalln("GOLANG UPSTREAM REGEXP BROKEN - REPORT!")
 	}
 
 	a := aUrl.String()
@@ -94,7 +126,6 @@ func main() {
 		log.Fatalln(err)
 	}
 	
-	
 	src, err := bittor.GetMainDict(tData)
 	if err != nil {
 		log.Fatalln("Error decoding ", tName, ":", err)
@@ -109,6 +140,71 @@ func main() {
 		log.Fatalln("Problems with hashing the info value of torrent: ", err)
 	}
 	
+	fmt.Println("hashy")
 	fmt.Printf("% x\n", tHash.Sum(nil))
 	fmt.Println(info["piece length"])
+	fmt.Println("hashy")
+	
+	peer_id := peerIdCreation()
+	fmt.Println(peer_id)
+	fmt.Println(string(peer_id))
+
+
+	
+	uploaded := 0;
+	downloaded := 0;
+	left := info["length"].(int)
+	compact := 0
+	event := "started"
+	
+	aUrl,err := url.Parse(string(src["announce"].(string)))
+	if err != nil {
+		log.Fatalln("Can't parse announce tracker url: ",err)
+	}
+	v := url.Values{}
+	v.Set("info_hash", string(tHash.Sum(nil)))
+	v.Set("peer_id", string(peer_id))
+	v.Set("uploaded", strconv.Itoa(uploaded))
+	v.Set("downloaded", strconv.Itoa(downloaded))
+	v.Set("left", strconv.Itoa(left))
+	v.Set("compact", strconv.Itoa(compact))
+	v.Set("event", event)
+	
+	aUrl.RawQuery = v.Encode()
+	fmt.Println(aUrl.String())
+	
+	fmt.Println("yo")
+	aResp, err := http.Get(aUrl.String())
+	if err != nil {
+		log.Fatalln("Error announcing to tracker: ", err)
+	}
+	text,_ := ioutil.ReadAll(aResp.Body)
+	fmt.Println(text)
+	fmt.Println("hej")
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
